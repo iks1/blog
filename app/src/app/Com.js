ce21836a37dad1f948f5d5940a0799fe21be441b
iks1 <rajinertia9239111@gmail.com>
2023-06-12 15:16:31 +0530

    added layout

---
+++ app/src/app/Com.js
@@ -0,0 +1,35 @@
+'use client';
+import Head from 'next/head'
+import PostCard from '../components/PostCard'
+import Categories from '../components/Categories'
+import PostWidget from '../components/PostWidget'
+import './globals.css'
+import Layout from '../components/Layout'
+
+const posts =[
+  {title:'Post 1',description:'Description 1'},
+  {title:'Post 2',description:'Description 2'},
+];
+
+export default function Com() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+         <div className='lg:col-span-8 col-span-1'>
+            {posts.map((post)=>(
+               <PostCard key={post.title} post={post}/>
+            ))}
+         </div>
+          
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+               <Categories/>
+               <PostWidget/>
+            </div>
+        </div>
+          
+         
+       </div>
+    </div>
+  )
+}
--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,35 +1,14 @@
 'use client';
-import Head from 'next/head'
-import PostCard from '../components/PostCard'
-import Categories from '../components/Categories'
-import PostWidget from '../components/PostWidget'
-
-const posts =[
-  {title:'Post 1',description:'Description 1'},
-  {title:'Post 2',description:'Description 2'},
-];
+import './globals.css'
+import Layout from '@/components/Layout';
+import Com from './Com'
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8">
-       <Head>
-        <title>bLOG</title>
-       </Head>
-       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-         <div className='lg:col-span-8 col-span-1'>
-            {posts.map((post)=>(
-               <PostCard key={post.title} post={post}/>
-            ))}
-         </div>
-          <div className='lg:col-span-4 col-span-1'>
-             <div className='lg:sticky relative top-8'>
-               <Categories/>
-               <PostWidget/>
-             </div>
-          </div>
-          
-         
-       </div>
-    </div>
+    <>
+    <Layout>
+      <Com/>
+    </Layout>
+    </>
   )
 }
